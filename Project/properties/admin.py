ec37a35fbea1802d11d910f16c3cda16fc12c62d
greeshma <modugumudigreeshma3@gmail.com>
2025-04-11 10:35:17 +0530

    updating to github

---
+++ Project/properties/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from .models import Property, Booking
+
+@admin.register(Property)
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = ['name', 'location', 'price', 'bedrooms', 'rating']
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('property', 'status', 'check_in_date', 'check_out_date', 'user_id', 'booking_date')
+
+admin.site.register(Booking, BookingAdmin)
+
+
---
+++ Project/properties/apps.py
@@ -0,0 +1,10 @@
+# from django.apps import AppConfig
+
+
+# class PropertiesConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'properties'
+
+
+#     def ready(self):
+#         pass  # Ensure no model imports here
